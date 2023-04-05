1d94193e39a46bb074a5199c2bebd089db7c7b34
Will Morrison <will.morrison@starburstdata.com>
2023-04-05 13:43:18 -0400

    add logic to check for updated rules file and synchronize on update

--- gateway-ha/src/main/java/com/lyft/data/gateway/ha/router/RuleCachingRoutingGroupSelector.java
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/router/RuleCachingRoutingGroupSelector.java
@@ -1,11 +1,13 @@
 package com.lyft.data.gateway.ha.router;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
-//import java.nio.file.attribute.FileTime;
+import java.nio.file.attribute.FileTime;
 import java.util.HashMap;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -27,16 +29,22 @@ public class RuleCachingRoutingGroupSelector
 
   RulesEngine rulesEngine = new DefaultRulesEngine();
   MVELRuleFactory ruleFactory = new MVELRuleFactory(new YamlRuleDefinitionReader());
+  String rulesConfigPath;
   Rules rules;
-  //FileTime lastUpdatedTime;
+  boolean rulesLoaded;
+  long lastUpdatedTime;
 
   RuleCachingRoutingGroupSelector(String rulesConfigPath) {
+    this.rulesConfigPath = rulesConfigPath;
+
     try {
-      //BasicFileAttributes attr = Files.readAttributes(Path.of(rulesConfigPath),
-      //        BasicFileAttributes.class);
-      //lastUpdatedTime = attr.lastModifiedTime();
       rules = ruleFactory.createRules(
               new FileReader(rulesConfigPath));
+      BasicFileAttributes attr = Files.readAttributes(Path.of(rulesConfigPath),
+              BasicFileAttributes.class);
+      lastUpdatedTime = attr.lastModifiedTime().toMillis();
+
+      rulesLoaded = true;
     } catch (Exception e) {
       log.error("Error opening rules configuration file, using "
               + "routing group header as default.", e);
@@ -45,11 +53,38 @@ public class RuleCachingRoutingGroupSelector
 
   @Override
   public String findRoutingGroup(HttpServletRequest request) {
-    Facts facts = new Facts();
-    HashMap<String, String> result = new HashMap<String, String>();
-    facts.put("request", request);
-    facts.put("result", result);
-    rulesEngine.fire(rules, facts);
-    return result.get("routingGroup");
+    try {
+      BasicFileAttributes attr = Files.readAttributes(Path.of(rulesConfigPath),
+              BasicFileAttributes.class);
+      log.debug(String.format("Current modified time %s, last modified time %s",
+              attr.lastModifiedTime().toMillis(), lastUpdatedTime));
+      if (attr.lastModifiedTime().toMillis() > lastUpdatedTime) {
+        log.info(String.format("Updating rules to file modified at %s", attr.lastModifiedTime()));
+        synchronized (this) {
+          // TODO: talk to the guys about whether or not we should synchronize
+          //  on this, rules, or sth else
+          rules = ruleFactory.createRules(
+                  new FileReader(rulesConfigPath));
+          lastUpdatedTime = attr.lastModifiedTime().toMillis();
+        }
+      }
+      rulesLoaded = true;
+    } catch (Exception e) {
+      log.error("Error opening rules configuration file, using "
+              + "routing group header as default.", e);
+      rulesLoaded = false;
+      // This could lead to perf problems as every thread goes into the synchronized
+      // block until the issue is resolved
+    }
+    if (rulesLoaded) {
+      Facts facts = new Facts();
+      HashMap<String, String> result = new HashMap<String, String>();
+      facts.put("request", request);
+      facts.put("result", result);
+      rulesEngine.fire(rules, facts);
+      return result.get("routingGroup");
+    }
+    return Optional.ofNullable(request.getHeader(ROUTING_GROUP_HEADER))
+        .orElse(request.getHeader(ALTERNATE_ROUTING_GROUP_HEADER));
   }
 }
