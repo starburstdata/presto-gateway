ecb874790c5e2bcae07c05d2f7db5d496e5ba99a
Willmo <wmorrison@macbook-pro.myfiosgateway.com>
2023-05-31 16:36:25 -0400

    merge changes

--- gateway-ha/src/main/java/com/lyft/data/gateway/ha/persistence/dao/QueryIdBackend.java
+++ gateway-ha/src/main/java/com/lyft/data/gateway/ha/persistence/dao/QueryIdBackend.java
@@ -1,14 +1,17 @@
 package com.lyft.data.gateway.ha.persistence.dao;
 
 import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Cached;
 import org.javalite.activejdbc.annotations.IdName;
 import org.javalite.activejdbc.annotations.Table;
 
 @IdName("queryid")
 @Table("queryid")
+@Cached
 public class QueryIdBackend extends Model {
   public static final String queryid = "queryid";
   public static final String backend = "backend";
+  public static final String createdTimestamp = "created_timestamp";
 
   public static void create(
       QueryIdBackend model,
@@ -16,6 +19,7 @@ public class QueryIdBackend extends Model {
       String backend) {
     model.set(QueryIdBackend.queryid, queryid);
     model.set(QueryIdBackend.backend, backend);
+    model.set(QueryIdBackend.createdTimestamp, System.currentTimeMillis());
     model.insert();
   }
 }
